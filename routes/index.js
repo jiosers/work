a9410f005b26897fccb6c1320cc9031a2600de83
jiosers <871481399@qq.com>
2018-05-09 22:48:00 +0800

    modify

--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,30 @@ var router = express.Router();
 /* GET home page. */
 let users={"june":"123456"};
 router.get('/', function(req, res, next) {
-	console.log(req.session.num)
-	req.session.num=req.session.num||0;
-	let num=++req.session.num;
- 
-  	res.render('index',{num})
+	let see=req.session;
+	if(see.user&&users[see.user.name]===see.user.password){
+		res.redirect('/chat')
+	}else{
+		res.render('index')
+	}
 });
 
 router.post("/login",(req,res,next)=>{
 	let {name,password}=req.body;
 	let state=0;
 	if(users[name]&&users[name]===password){
-		//req.session.user={name,password};
+		req.session['user']={name,password};
 		state=1;
 	}
-	console.log("num:",req.session.num)
-	req.session.num=req.session.num||0;
-	let num=++req.session.num;
+	/*req.session.num=req.session.num||0;
+	let num=++req.session.num;*/
+	console.log("req.session:",req.session);
 	res.send({state})
 })
 //聊天室界面
 router.get('/chat',(req,res,next)=>{
 	//res.locals.user=req.session.user||""
+	console.log("req:",req.session);
 	if(req.session.user){
   	res.render("chat");
   }else{
